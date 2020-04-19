fb3c8917dc6216ab50d79a36ca065a324ec8bfa4
shihab <echoshihab@gmail.com>
2020-04-18 21:52:24 -0400

    autoimplemented property initializer + string interpolation

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace LanguageFeatures.Controllers
                 string name = p?.Name ?? "<No Name>";
                 decimal? price = p?.Price ?? 0;
                 string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+                results.Add($"Name: {name}, Price: {price:C2}, Related: {relatedName}");
 
             }
             return View(results);
