1ab5c5271b8e02a3172dee1c16e7f127b0f7fe4d
shihab <echoshihab@gmail.com>
2020-04-22 15:43:00 -0400

    fix viewstart + update view through conditionals

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace Razor.Controllers
                 Category = "Watersports",
                 Price = 275M
             };
+            ViewBag.StockLevel = 2;
 
             return View(myProduct);
         }
