7a0f0335956c7991c686e783ba6f00289c4944bf
shihab <echoshihab@gmail.com>
2020-04-23 13:54:31 -0400

    display products thru in-memory repository

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -9,19 +9,18 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public IActionResult Index()
         {
-            Product myProduct = new Product
-            {
-                ProductId = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "Watersports",
-                Price = 275M
-            };
-            ViewBag.StockLevel = 2;
+            Product[] array =
+             {
+                new Product {Name = "Kayak", Price=275M},
+                new Product {Name = "Lifejacket", Price=48.95M},
+                new Product {Name = "Soccer Ball", Price=19.50M},
+                new Product {Name = "Corner Flag", Price=34.95M},
 
-            return View(myProduct);
+            };
+
+            return View(array);
         }
     }
 }
