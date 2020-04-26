a1d5a15b21bc7882cac0717217b6de2f6d7c3702
shihab <echoshihab@gmail.com>
2020-04-25 21:10:32 -0400

    add browserlink with runtime compilation

--- WorkingWithVisualStudio/Startup.cs
+++ WorkingWithVisualStudio/Startup.cs
@@ -16,7 +16,8 @@ namespace WorkingWithVisualStudio
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +26,8 @@ namespace WorkingWithVisualStudio
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStaticFiles();
+               
             }
 
             app.UseRouting();
