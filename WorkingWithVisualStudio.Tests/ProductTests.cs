9fceef8d9ef1d514930b9d13581aa743148aed70
shihab <echoshihab@gmail.com>
2020-04-27 12:10:39 -0400

    add unit tests

---
+++ WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkingWithVisualStudio.Model;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            //Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            //ACT
+            p.Name = "New Name";
+
+            //Assert
+            Assert.Equal("New Name", p.Name);
+        }
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            //ARRANGE
+            var p = new Product { Name = "Test", Price = 100M };
+
+            //Act
+            p.Price = 200M;
+
+            //Assert
+            Assert.Equal(200M, p.Price);
+
+        }
+
+    }
+}
