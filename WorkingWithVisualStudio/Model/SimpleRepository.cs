9a61b82f49131227f69b5d6801437fc634b10ef1
shihab <echoshihab@gmail.com>
2020-04-28 16:39:27 -0400

    add controller tests

--- WorkingWithVisualStudio/Model/SimpleRepository.cs
+++ WorkingWithVisualStudio/Model/SimpleRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace WorkingWithVisualStudio.Model
 {
-    public class SimpleRepository
+    public class SimpleRepository : IRepository
     {
         private static SimpleRepository sharedRepository = new SimpleRepository();
         private Dictionary<string, Product> products = new Dictionary<string, Product>();
